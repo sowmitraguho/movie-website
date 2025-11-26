9e6109710ad27b9fe5a9b0a8c0eebb9461e337c7
Sowmitra Guha <sowmmochy@gmail.com>
2025-11-27 04:43:00 +0600

    type fix

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,24 @@
-// lib/utils.ts
-export function cn(...classes: string[]) {
-    return classes.filter(Boolean).join(' ');
+// utils.ts
+type ClassValue = string | undefined | null | false;
+
+export function cn(...classes: ClassValue[]) {
+  return classes.filter(Boolean).join(' ');
+}
+
+// Variant helper (basic)
+type VariantOptions<T extends string> = {
+  [K in T]?: string;
+};
+
+type VariantProps<T extends string> = {
+  variant?: T;
+};
+
+export function withVariant<T extends string>(
+  baseClass: string,
+  variants: VariantOptions<T>
+) {
+  return ({ variant, className }: VariantProps<T> & { className?: string } = {}) => {
+    return cn(baseClass, variant ? variants[variant] : undefined, className);
+  };
 }
